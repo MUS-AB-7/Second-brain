5f821df01c2a185647b44ad6e8c8a64096c76228
MUS-AB-7 <2k22.cse.2213268@gmail.com>
2025-02-21 11:29:00 +0530

    LoGo updated

--- brainlyFrontend/src/components/sidebar.tsx
+++ brainlyFrontend/src/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "../icons/Logo";
 import { TwitterIcon } from "../icons/TwitterIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 import { SidebarIten } from "./SidebarItem";
@@ -5,6 +6,7 @@ import { SidebarIten } from "./SidebarItem";
 export function Sidebar(){
     return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 pl-6">
         <div className="flex text-2xl pt-4">
+            <Logo />
             Brainly
         </div>
         <div className="pt-4">
